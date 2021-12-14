85f365c4f2c0b29c57e0a866c1606d198642b8f8
yurystar <staradubtsau_yury@mail.ru>
2021-12-14 12:51:06 +0300

    added task-2/1

---
+++ Task 2/task-2-1/Greeting.java
@@ -0,0 +1,9 @@
+public class Greeting {
+    static void print() {
+        System.out.println("Hello java!");
+    }
+
+    public static void main(String[] args) {
+        Greeting.print();
+    }
+}
