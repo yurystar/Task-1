3137359b8169e3b8cc1b6f0289b2bb9a4543d9ee
yurystar <staradubtsau_yury@mail.ru>
2022-01-24 16:08:44 +0300

    drl file 6-7

--- Task 6-7/Task-6-7-1/entity/RoomStatus.java
+++
@@ -1,5 +0,0 @@
-package com.senla.elhoteladmin.entity;
-
-public enum RoomStatus {
-    EMPTY, ON_REPAIR, BUSY
-}
--- Task 6-7/Task-6-7-1/entity/RoomType.java
+++
@@ -1,5 +0,0 @@
-package com.senla.elhoteladmin.entity;
-
-public enum RoomType {
-    STARS_2, STARS_3, STARS_4
-}
